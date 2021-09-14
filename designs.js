a68d6a545cc8c2fe8ac6aa0c3fd93315db2fff33
Josi Kie <kiejosi12@gmail.com>
2021-09-14 14:37:16 +0700

    Second commit

--- designs.js
+++ designs.js
@@ -1,38 +1,41 @@
-// Select color input
-const color = document.querySelector('#colorPicker');
-
-// Select size input
-const height = document.querySelector('#inputHeight');
-const width = document.querySelector('#inputWidth');
-
 // When size is submitted by the user, call makeGrid()
 const form = document.getElementById('sizePicker');
+let tables = document.querySelector('table');
+
 form.addEventListener('submit', makeGrid);
 
-// TODO: make canvas for user
+/**
+ * Add rows and columns
+ * @param {*} event 
+ */
 function makeGrid(event) {
+
     //prevent the default action  
     event.preventDefault();
+
+    // Select color input
+    const color = document.querySelector('#colorPicker');
+
+    // Select size input
+    const height = document.querySelector('#inputHeight');
+    const width = document.querySelector('#inputWidth');
+
+    tables.innerHTML = "";
 
     // TODO: add rows and columns
     for(let h = 1; h <= height.value; h++){
     
         // TODO: add row
         let tr = document.createElement('tr');
-        let table = document.querySelector('#pixelCanvas');
-        table.appendChild(tr);
+        tables.appendChild(tr);
 
         // TODO: add column
         for(let w = 1; w <= width.value; w++){
             let td = document.createElement('td');
             tr.appendChild(td);
-            td.addEventListener('click', function(event){
-            td.style.background = color.value;
-
-            // TODO: reset to blank 
-            if(color.value !== ''){
-                form.removeEventListener('submit',makeGrid);
-            }});
+            td.addEventListener('click', function(event){  
+              td.style.backgroundColor = color.value;
+            });
         }   
     }
 
